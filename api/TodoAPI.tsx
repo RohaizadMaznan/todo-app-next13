63251a739863a7692e02731417dcee84bea4b4f6
Rohaizad ZNX <rohaizad.maznan@zionext.com>
2023-01-17 16:29:22 +0800

    Added pocketbase hostname

--- api/TodoAPI.tsx
+++ api/TodoAPI.tsx
@@ -1,4 +1,5 @@
 const HOST_NAME = "http://127.0.0.1";
 const PORT = "8090";
 
-export const HOST_URL = `${HOST_NAME}:${PORT}/api/collections`;
+// export const HOST_URL = `${HOST_NAME}:${PORT}/api/collections`;
+export const HOST_URL = "https://todo-app-next13.fly.dev/api/collections";
